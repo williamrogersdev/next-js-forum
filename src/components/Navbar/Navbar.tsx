dac2d0708ac88dff251effe229ac6c6137cf7a09
William Rogers <williamrogersdev@gmail.com>
2023-02-22 16:05:37 -0600

    fixed bug with styles

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,19 @@
+import { Flex, Image } from '@chakra-ui/react';
 import React from 'react';
 
 
 
 const Navbar:React.FC= () => {
     
-    return <div>Navbar</div>
+    return (
+
+        <Flex bg="white" height="44px" padding="6px 12px">
+            <Flex>
+                <Image src="/images/redditFace.svg" />
+                <Image />
+            </Flex>
+            Nav
+        </Flex>
+    )
 }
 export default Navbar;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import { theme } from "../chakra/theme";
 import Layout from "../components/Layout/Layout";
-import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
