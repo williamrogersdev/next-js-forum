345082b38aaae9c18a5c6b26f726a8957c0bd08c
William Rogers <williamrogersdev@gmail.com>
2023-02-22 21:17:54 -0600

    added modal from chakra

---
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -0,0 +1,39 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+type AuthModalProps = {};
+
+const AuthModal: React.FC<AuthModalProps> = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button onClick={onOpen}>Open Modal</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>HIIIIII</ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+export default AuthModal;
--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Flex } from "@chakra-ui/react";
 import AuthButtons from "./AuthButtons";
+import AuthModal from "@/components/Modal/Auth/AuthModal";
 
 type RightContentProps = {
  
@@ -9,7 +10,7 @@ type RightContentProps = {
 const RightContent: React.FC<RightContentProps> = () => {
   return (
     <>
-      {/* <AuthModal /> */}
+     <AuthModal/>
       <Flex justifyContent="space-between" alignItems="center">
         <AuthButtons/>
       </Flex>
