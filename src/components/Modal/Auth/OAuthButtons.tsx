60cd1b4d0557f184217258e2d03960b0cd051fa4
William Rogers <williamrogersdev@gmail.com>
2023-02-26 00:46:35 -0600

    created sign in with google functionality

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,17 +1,26 @@
-import { Button, Flex, Image } from '@chakra-ui/react';
+import { Button, Flex, Image, Text } from '@chakra-ui/react';
 import React from 'react';
-
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { auth } from '@/firebase/clientApp';
 
 
 const OAuthButtons:React.FC = () => {
+
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
     
 
     return (
-        <Flex direction="column" width="100%" mb={4} >
-            <Button variant="oauth" mb={2}>
+        <Flex direction="column" width="100%" mb={4}>
+            <Button variant="oauth" 
+            mb={2} 
+            isLoading={loading} 
+            onClick={()=> {signInWithGoogle()}}
+            
+            >
                 <Image src="./images/googlelogo.png" alt="google" width="20px" height="20px" mr={2} /> Login with Google
             </Button>
             <Button variant="oauth">Other Login</Button>
+            {error && <Text>Error</Text>}
         </Flex>
     )
 }
