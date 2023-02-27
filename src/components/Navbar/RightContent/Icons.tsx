a5a96e781d2e9c2ede861e434f434df8d6e1da69
William Rogers <williamrogersdev@gmail.com>
2023-02-26 20:41:46 -0600

    added rest of icons

--- src/components/Navbar/RightContent/Icons.tsx
+++ src/components/Navbar/RightContent/Icons.tsx
@@ -1,7 +1,8 @@
-import { Flex, Icon } from "@chakra-ui/react";
-import { BsArrowUpRightCircle, BsChatDots } from "react-icons/bs";
 
 import React from "react";
+import { AddIcon } from "@chakra-ui/icons";
+import { Box, Flex, Icon } from "@chakra-ui/react";
+import { BsArrowUpRightCircle, BsChatDots } from "react-icons/bs";
 import { GrAdd } from "react-icons/gr";
 import {
   IoFilterCircleOutline,
@@ -9,48 +10,81 @@ import {
   IoVideocamOutline,
 } from "react-icons/io5";
 
+
 const Icons: React.FC = () => {
-  return (
-    <Flex>
-      <Flex
-        display={{ base: "none", md: "flex" }}
-        align="center"
-        borderRight="11 px solid "
-        borderColor="gray.200"
-      >
-      </Flex>
-      <Flex
-        mr={1.5}
-        ml={1.5}
-        padding={1}
-        cursor="pointer"
-        borderRadius={4}
-        _hover={{ bg: "gray.200" }}
-      >
-        <Icon as={BsArrowUpRightCircle} fontSize={20} />
-      </Flex>
-      <Flex
-          mr={1.5}
-          ml={1.5}
-          padding={1}
-          cursor="pointer"
-          borderRadius={4}
-          _hover={{ bg: "gray.200" }}
+    return (
+      <Flex alignItems="center" flexGrow={1}>
+        <Box
+          display={{ base: "none", md: "flex" }}
+          alignItems="center"
+          borderRight="1px solid"
+          borderColor="gray.200"
         >
-          <Icon as={IoFilterCircleOutline} fontSize={22} />
-        </Flex>
-        <Flex
-          mr={1.5}
-          ml={1.5}
-          padding={1}
-          cursor="pointer"
-          borderRadius={4}
-          _hover={{ bg: "gray.200" }}
-        >
-          <Icon as={IoVideocamOutline} fontSize={22} />
-        </Flex>
-      <></>
-    </Flex>
-  );
-};
+          <Flex
+            mr={1.5}
+            ml={1.5}
+            padding={1}
+            cursor="pointer"
+            borderRadius={4}
+            _hover={{ bg: "gray.200" }}
+          >
+            <Icon as={BsArrowUpRightCircle} fontSize={20} />
+          </Flex>
+          <Flex
+            mr={1.5}
+            ml={1.5}
+            padding={1}
+            cursor="pointer"
+            borderRadius={4}
+            _hover={{ bg: "gray.200" }}
+          >
+            <Icon as={IoFilterCircleOutline} fontSize={22} />
+          </Flex>
+          <Flex
+            mr={1.5}
+            ml={1.5}
+            padding={1}
+            cursor="pointer"
+            borderRadius={4}
+            _hover={{ bg: "gray.200" }}
+          >
+            <Icon as={IoVideocamOutline} fontSize={22} />
+          </Flex>
+        </Box>
+        <>
+          <Flex
+            mr={1.5}
+            ml={1.5}
+            padding={1}
+            cursor="pointer"
+            borderRadius={4}
+            _hover={{ bg: "gray.200" }}
+          >
+            <Icon as={BsChatDots} fontSize={20} />
+          </Flex>
+          <Flex
+            mr={1.5}
+            ml={1.5}
+            padding={1}
+            cursor="pointer"
+            borderRadius={4}
+            _hover={{ bg: "gray.200" }}
+          >
+            <Icon as={IoNotificationsOutline} fontSize={20} />
+          </Flex>
+          <Flex
+            display={{ base: "none", md: "flex" }}
+            mr={3}
+            ml={1.5}
+            padding={1}
+            cursor="pointer"
+            borderRadius={4}
+            _hover={{ bg: "gray.200" }}
+          >
+            <Icon as={GrAdd} fontSize={20} />
+          </Flex>
+        </>
+      </Flex>
+    );
+  };
 export default Icons;
