c31b419d8a4584cce300293ba570af4aaa988ee2
William Rogers <williamrogersdev@gmail.com>
2023-02-26 20:28:15 -0600

    removed signout button and replcaed with icons component

---
+++ src/components/Navbar/RightContent/Icons.tsx
@@ -0,0 +1,18 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+import { GrAdd } from "react-icons/gr";
+import {
+  IoFilterCircleOutline,
+  IoNotificationsOutline,
+  IoVideocamOutline,
+} from "react-icons/io5";
+
+const Icons: React.FC = () => {
+  return (
+    <Flex>
+      <Flex>Icons</Flex>
+      <></>
+    </Flex>
+  );
+};
+export default Icons;
--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -2,20 +2,20 @@ import React from "react";
 import { Button, Flex } from "@chakra-ui/react";
 import AuthButtons from "./AuthButtons";
 import AuthModal from "@/components/Modal/Auth/AuthModal";
-import { signOut } from "firebase/auth";
+import { signOut, User } from "firebase/auth";
 import { auth } from "@/firebase/clientApp";
+import Icons from "./Icons";
 
 type RightContentProps = {
- 
-  user: any;
+  user?: User | null;
 };
 
-const RightContent: React.FC<RightContentProps> = ({user}) => {
+const RightContent: React.FC<RightContentProps> = ({ user }) => {
   return (
     <>
-     <AuthModal/>
+      <AuthModal />
       <Flex justifyContent="space-between" alignItems="center">
-        {user ?  <Button onClick={() => signOut(auth)}>Sign Out</Button> : <AuthButtons/>}
+        {user ? <Icons /> : <AuthButtons />}
       </Flex>
     </>
   );
