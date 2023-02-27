5e5a5932e6e9aadae4b89d8b59a454c2d4400450
William Rogers <williamrogersdev@gmail.com>
2023-02-26 20:37:01 -0600

    added new icons to the nav bar

--- src/components/Navbar/RightContent/Icons.tsx
+++ src/components/Navbar/RightContent/Icons.tsx
@@ -1,4 +1,6 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Icon } from "@chakra-ui/react";
+import { BsArrowUpRightCircle, BsChatDots } from "react-icons/bs";
+
 import React from "react";
 import { GrAdd } from "react-icons/gr";
 import {
@@ -10,7 +12,43 @@ import {
 const Icons: React.FC = () => {
   return (
     <Flex>
-      <Flex>Icons</Flex>
+      <Flex
+        display={{ base: "none", md: "flex" }}
+        align="center"
+        borderRight="11 px solid "
+        borderColor="gray.200"
+      >
+      </Flex>
+      <Flex
+        mr={1.5}
+        ml={1.5}
+        padding={1}
+        cursor="pointer"
+        borderRadius={4}
+        _hover={{ bg: "gray.200" }}
+      >
+        <Icon as={BsArrowUpRightCircle} fontSize={20} />
+      </Flex>
+      <Flex
+          mr={1.5}
+          ml={1.5}
+          padding={1}
+          cursor="pointer"
+          borderRadius={4}
+          _hover={{ bg: "gray.200" }}
+        >
+          <Icon as={IoFilterCircleOutline} fontSize={22} />
+        </Flex>
+        <Flex
+          mr={1.5}
+          ml={1.5}
+          padding={1}
+          cursor="pointer"
+          borderRadius={4}
+          _hover={{ bg: "gray.200" }}
+        >
+          <Icon as={IoVideocamOutline} fontSize={22} />
+        </Flex>
       <></>
     </Flex>
   );
