0074ca664e22b800ce4f9aa799483aa5319fb318
William Rogers <williamrogersdev@gmail.com>
2023-03-02 14:52:16 -0600

    added home dir and styled it

--- src/components/Navbar/Directory/Directory.tsx
+++ src/components/Navbar/Directory/Directory.tsx
@@ -1,11 +1,9 @@
 import { authModalState } from "@/atoms/authModalAtom";
 import { ChevronDownIcon } from "@chakra-ui/icons";
-import {
-    Flex, Menu,
-    MenuButton, MenuList
-} from "@chakra-ui/react";
+import { Flex, Menu, MenuButton, MenuList, Icon, Text } from "@chakra-ui/react";
 import React from "react";
 import { useSetRecoilState } from "recoil";
+import { TiHome } from "react-icons/ti";
 
 const Directory: React.FC<UserMenuProps> = () => {
   const setAuthModalState = useSetRecoilState(authModalState);
@@ -16,10 +14,17 @@ const Directory: React.FC<UserMenuProps> = () => {
         cursor="pointer"
         padding="0px 6px"
         border={4}
+        mr={2}
+        ml={{ base: 0, md: 2 }}
         _hover={{ outline: "1px solid", outlineColor: "gray.300" }}
       >
         <Flex align="center">
-          <Flex align="center"></Flex>
+          <Flex align="center">
+            <Icon fontSize={24} mr={{ base: 1, md: 2 }} as={TiHome} />
+            <Flex display={{ base: "none", lg: "flex" }}>
+              <Text fontWeight={600}>Home</Text>
+            </Flex>
+          </Flex>
           <ChevronDownIcon />
         </Flex>
       </MenuButton>
