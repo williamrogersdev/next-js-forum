4f13ce0d1d8aa6f68403d0b4b1f1382c6809bc5b
William Rogers <williamrogersdev@gmail.com>
2023-03-02 14:12:07 -0600

    added profile menu item

--- src/components/Navbar/RightContent/UserMenu.tsx
+++ src/components/Navbar/RightContent/UserMenu.tsx
@@ -12,6 +12,7 @@ import { FaRedditSquare } from "react-icons/fa";
 import { User } from "firebase/auth";
 import { VscAccount } from "react-icons/vsc";
 import { IoSparkles } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
 import React from "react";
 
 type UserMenuProps = {
@@ -25,7 +26,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
         cursor="pointer"
         padding="0px 6px"
         border={4}
-        _hover={{ outline: "1px solid", outlineColor: "gray.300"}}
+        _hover={{ outline: "1px solid", outlineColor: "gray.300" }}
       >
         {user ? (
           <Flex align="center">
@@ -42,12 +43,20 @@ const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
             </Flex>
           </Flex>
         ) : (
-          <Icon as={VscAccount} fontSize={24} mr={1} color="gray.300" />
+          <Icon as={VscAccount} fontSize={24} mr={1}  />
         )}
       </MenuButton>
       <MenuList>
-        <MenuItem>Download</MenuItem>
-
+        <MenuItem
+          fontSize="10pt"
+          fontWeight={700}
+          _hover={{ bg: "blue.500", color: "white" }}
+        >
+          <Flex align="center">
+            <Icon as={CgProfile} fontSize={20} mr={2} color="gray.300" />
+            Profile
+          </Flex>
+        </MenuItem>
       </MenuList>
     </Menu>
   );
