a515187b15e6523ae9b91248b5e9df83301855d9
William Rogers <williamrogersdev@gmail.com>
2023-03-02 22:50:06 -0600

    added modal bpdy and state for input boc

--- src/components/Modal/CreateCommunity/CreateCommunityModal.tsx
+++ src/components/Modal/CreateCommunity/CreateCommunityModal.tsx
@@ -1,5 +1,17 @@
-import { Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from "@chakra-ui/react";
-import React from "react";
+import {
+  Box,
+  Button,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
 
 type CreateCommunityModalProps = {
   open: boolean;
@@ -8,29 +20,54 @@ type CreateCommunityModalProps = {
 
 const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({
   open,
-    handleClose,
+  handleClose,
 }) => {
-    return (
-        <>
-          
-          <Modal isOpen={open} onClose={handleClose}>
-            <ModalOverlay    />
-            <ModalContent>
-              <ModalHeader>Modal Title</ModalHeader>
-              <ModalCloseButton />
-              <ModalBody>
-                MODAl BODY
-              </ModalBody>
-    
-              <ModalFooter>
-                <Button colorScheme='blue' mr={3} onClick={handleClose}>
-                  Close
-                </Button>
-                <Button variant='ghost'>Secondary Action</Button>
-              </ModalFooter>
-            </ModalContent>
-          </Modal>
-        </>
-      )
+
+const [communityName, setCommunityName] = useState("");
+
+  return (
+    <>
+      <Modal isOpen={open} onClose={handleClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader
+            display="flex"
+            flexDirection="column"
+            fontSize={15}
+            padding={3}
+          >
+            Create A Community
+          </ModalHeader>
+          <Box pl={3} pr={3}>
+            <ModalCloseButton />
+            <ModalBody
+              display="flex"
+              flexDirection="column"
+              padding="10px 0px"
+              border="1px solid red"
+            >
+              <Text fontWeight={600} fontSize={15}>
+                Community Name
+              </Text>
+              <Text fontSize={11} color="gray.500">
+                Community Names Cannot Be Changed
+              </Text>
+              <Text>
+                r/
+              </Text>
+              <Input />
+            </ModalBody>
+          </Box>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Create Community</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 };
 export default CreateCommunityModal;
