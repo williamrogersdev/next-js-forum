93819669a0d08343e6a99442426b61a35c8368b1
William Rogers <williamrogersdev@gmail.com>
2023-03-04 00:19:11 -0600

    set up user database

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,18 @@
 import * as functions from "firebase-functions";
 
+import * as admin from "firebase-admin";
+admin.initializeApp();
+
+const db = admin.firestore();
+
+export const createUserDocument = functions.database
+  .user()
+  .onCreate(async (user) => {
+    db.collection("users")
+      .doc(user.uid)
+      .set(JSON.parse(JSON.stringify(user)));
+  });
+
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 //
